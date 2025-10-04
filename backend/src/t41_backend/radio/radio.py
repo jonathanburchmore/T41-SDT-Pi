f4acfbf00817f7d3663ef01c40d9522d5a2cee9a
Jonathan Burchmore <jonathanburchmore@gmail.com>
2025-10-03 19:03:06 -0700

    Formatting, moved management/shutdown of tasks into base Radio class, startup into derived classes

--- backend/src/t41_backend/radio/radio.py
+++ backend/src/t41_backend/radio/radio.py
@@ -23,53 +23,53 @@ class IQBuffer:
                 
             await queue.put(block)
 
+
 class Radio(ABC):
     def __init__(self, sample_rate=192000):
         self.iq = IQBuffer(sample_rate=sample_rate)
+        self._tasks = {}
 
     async def startup(self):
-        self.rx_iq_task = asyncio.create_task( self._rx_iq_job() )
+        pass
 
     async def shutdown(self):
-        self.rx_iq_task.cancel()
+        for task in self._tasks.values():
+            task.cancel()
+            await task
 
-        try:
-            await self.rx_iq_task
-        except Exception:
-            pass
-
-    @abstractmethod
-    async def _rx_iq_job(self):
-        pass
 
 class DummyRadio(Radio):
     def __init__(self):
         super().__init__(sample_rate=192000)
         self._gqrx_file = "gqrx_20250930_023302_128975000_192000_fc.raw"
 
-    async def _rx_iq_job(self):
+    async def startup(self):
+        await super().startup()
+        self._tasks["rx_iq"] = asyncio.create_task(self._rx_iq())
+
+    async def _rx_iq(self):
         try:
             # Calculate delay to simulate "real" sample capture rate
             block_time = self.iq.block_size / self.iq.sample_rate
 
             # Replay the same file over and over, endlessly    
             while True:
-                async with aiofiles.open( self._gqrx_file, "rb" ) as input:
+                async with aiofiles.open(self._gqrx_file, "rb") as input:
                     while True:
-                        raw = await input.read( self.iq.block_size * 2 * 4 )
+                        raw = await input.read(self.iq.block_size * 2 * 4)
                         if len( raw ) < self.iq.block_size * 2 * 4:
                             break
 
                         # Unpack interleaved I/Q samples and convert to complex64
-                        raw = np.frombuffer( raw, dtype='<f4' )
-                        iq_block = np.empty( self.iq.block_size, dtype='complex64' )
-                        iq_block[ : ] = raw[ 0::2 ] + 1j * raw[ 1::2 ]
+                        raw = np.frombuffer(raw, dtype='<f4')
+                        iq_block = np.empty(self.iq.block_size, dtype='complex64')
+                        iq_block[:] = raw[0::2] + 1j * raw[1::2]
 
                         # Queue block
-                        await self.iq.put( iq_block )
+                        await self.iq.put(iq_block)
 
-                        await asyncio.sleep( block_time )
+                        await asyncio.sleep(block_time)
         except asyncio.CancelledError:
             pass
         except Exception as e:
-            print( e )
+            print(f"DummyRadio::rx_iq: Exception: {e}")
