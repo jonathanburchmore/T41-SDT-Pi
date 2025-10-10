8f57f458d06c26ee545a08b18172894709fc37b7
Jonathan Burchmore <jonathanburchmore@gmail.com>
2025-10-09 18:31:27 -0700

    Converted to sounddevice, fixed several issues.

--- backend/src/t41_backend/radio/mchstreamer.py
+++ backend/src/t41_backend/radio/mchstreamer.py
@@ -1,33 +1,47 @@
 import asyncio
 import aiofiles
-import soundcard as sc
+import sounddevice as sd
 import numpy as np
 
 from .radio import Radio
 from .events import RadioEvents
 
 class MCHStreamer(Radio):
-    def __init__(self, iq, audio_device="MCHStreamer Lite I2S TosLink Multichannel 10", rx_i=0, rx_q=0, sample_rate=192000):
+    def __init__(self, device=1, rx_i=0, rx_q=1, sample_rate=192000):
         super().__init__(sample_rate=sample_rate)
-        self._audio_device = audio_device
+        self._device = device
         self._rx_i = rx_i
         self._rx_q = rx_q
+        self._stream = None
 
     async def startup(self):
         await super().startup()
-        self._tasks["rx_iq"] = asyncio.create_task(self._rx_iq())
 
-    async def _rx_iq(self):
-        try:
-            mic = sc.get_microphone(self._audio_device)
-            input = mic.recorder(samplerate=self._iq.sample_rate, channels=10)
+        self._loop = asyncio.get_running_loop()
 
-            while True:
-                data = await asyncio.to_thread(input.record, numframes=self._iq.block_size)
-                iq_block = (data[:, self._rx_i] + 1j * data[:, self._rx_q]).astype(np.complex64)
+        self._stream = sd.InputStream(
+            device=self._device,
+            channels=10,
+            samplerate=self.iq.sample_rate,
+            blocksize=self.iq.block_size,
+            dtype='int32',
+            callback=self._rx_iq
+        )
+        self._stream.start()
 
-                await self.iq.put(iq_block)
-        except asyncio.CancelledError:
-            pass
-        except Exception as e:
-            print(f"MCHStreamer::rx_iq: Exception: {e}")
+    async def shutdown(self):
+        if self._stream:
+            self._stream.stop()
+            self._stream.close()
+            self._stream = None
+
+        await super().shutdown()
+
+    def _rx_iq(self, indata, frames, time, status):
+        if status:
+            print(f"MCHStreamer::_rx_iq: status: {status}", flush=True)
+
+        iq_block = (indata[:, self._rx_i].astype(np.float32) + 1j * indata[:, self._rx_q].astype(np.float32))
+        iq_block = iq_block.astype(np.complex64)
+
+        self._loop.call_soon_threadsafe(self.iq.put_nowait, iq_block)
